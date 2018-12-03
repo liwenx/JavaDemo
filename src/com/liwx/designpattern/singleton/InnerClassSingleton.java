0edf16bc698832ad26d2b688a6899d03c7efcf2d
黎文兴 <liwenxing@hualala.com>
2018-12-03 20:48:00 +0800

    singleton

--- src/com/liwx/designpattern/singleton/InnerClassSingleton.java
+++ src/com/liwx/designpattern/singleton/InnerClassSingleton.java
@@ -7,6 +7,9 @@ package com.liwx.designpattern.singleton;
 
 /**
  * 通过静态内部类的方式 实现懒汉式单例 线程安全
+ *
+ * Java静态内部类的特性是，加载的时候不会加载内部静态类，使用的时候才会进行加载。而使用到的时候类加载又是线程安全的，
+ *
  */
 public class InnerClassSingleton {
     //内部类方式
